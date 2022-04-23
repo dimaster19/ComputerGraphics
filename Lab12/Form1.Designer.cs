9c76dbda2a7fc79faf84ffcc20c9df89cb5e0d7c
dimaster19 <91622671+dimaster19@users.noreply.github.com>
2022-04-23 23:32:08 +0300

    Add files via upload

---
+++ Lab12/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace Lab12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.YtextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.XtextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(829, 378);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(314, 431);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(217, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Сгенерировать B-сплайн";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(569, 424);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(250, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Введите точки Y через пробел ";
+            // 
+            // YtextBox
+            // 
+            this.YtextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.YtextBox.Location = new System.Drawing.Point(573, 447);
+            this.YtextBox.Name = "YtextBox";
+            this.YtextBox.Size = new System.Drawing.Size(265, 29);
+            this.YtextBox.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(11, 424);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Введите точки X через пробел ";
+            // 
+            // XtextBox
+            // 
+            this.XtextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.XtextBox.Location = new System.Drawing.Point(15, 447);
+            this.XtextBox.Name = "XtextBox";
+            this.XtextBox.Size = new System.Drawing.Size(265, 29);
+            this.XtextBox.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(293, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(238, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Вариант 7 (Вводить 25 точек)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(537, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "но вообще сколько хотите";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 485);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.XtextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.YtextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox YtextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox XtextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ Lab12/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab12
+{
+    public partial class Form1 : Form
+    {
+        Graphics graphics;
+        //double[] IntX = new double[25];
+        //double[] IntY = new double[25];
+
+        double eps = 0.04;
+        double X, Y, t, xA, xB, xC, xD, yA, yB, yC, yD, a0, a1, a2, a3, b0, b1, b2, b3;
+        int first;
+        public Form1()
+        {
+            InitializeComponent();
+            graphics = pictureBox1.CreateGraphics();
+
+        }
+        private int IX(double IntX)
+        {
+            double xx = IntX * (pictureBox1.Size.Width / 10.0) + 0.5;
+            return (int)xx;
+        }
+        private int IY(double IntY)
+        {
+            double yy = pictureBox1.Size.Height - IntY * (pictureBox1.Size.Height / 7.0) + 0.5;
+
+            return (int)yy;
+        }
+        private void Draw(double x1, double y1, double x2, double y2)
+        {
+            Point point1 = new Point(IX(x1), IY(y1));
+            Point point2 = new Point(IX(x2), IY(y2));
+            graphics.DrawLine(Pens.Black, point1, point2);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            graphics.Clear(Color.White);
+            double Xold = 0;
+            double Yold = 0;
+            string[] StringX = XtextBox.Text.Split(' ');
+
+            int[] IntX = new int[StringX.Length];
+            for (int i = 0; i < StringX.Length; i++)
+            {
+                IntX[i] = Convert.ToInt32(StringX[i]);
+            }
+
+            string[] StringY = YtextBox.Text.Split(' ');
+
+            int[] IntY = new int[StringY.Length];
+            for (int i = 0; i < StringY.Length; i++)
+            {
+                IntY[i] = Convert.ToInt32(StringY[i]);
+            }
+
+
+            //for (int i=0; i<40; i++)
+            //{
+            //    IntX[i] = random.Next(1, 10) + random.NextDouble();
+            //    IntY[i] = random.Next(1, 7) + random.NextDouble();
+            //}
+            for (int i = 0; i < IntX.Length; i++)
+            {
+                X = IntX[i];
+                Y = IntY[i];
+                Draw(X - eps, Y - eps, X + eps, Y + eps);
+                Draw(X + eps, Y - eps, X - eps, Y + eps);
+            }
+            first = 1;
+            for (int i = 1; i < IntX.Length - 2; i++)
+            {
+                xA = IntX[i - 1];
+                xB = IntX[i];
+                xC = IntX[i + 1];
+                xD = IntX[i + 2];
+                yA = IntY[i - 1];
+                yB = IntY[i];
+                yC = IntY[i + 1];
+                yD = IntY[i + 2];
+                a3 = (-xA + 3 * (xB - xC) + xD) / 6.0;
+                b3 = (-yA + 3 * (yB - yC) + yD) / 6.0;
+                a2 = (xA - 2 * xB + xC) / 2.0;
+                b2 = (yA - 2 * yB + yC) / 2.0;
+                a1 = (xC - xA) / 2.0;
+                b1 = (yC - yA) / 2.0;
+                a0 = (xA + 4 * xB + xC) / 6.0;
+                b0 = (yA + 4 * yB + yC) / 6.0;
+                for (int j = 0; j <= IntX.Length; j++)
+                {
+                    t = (double)j / (double)IntX.Length;
+                    X = ((a3 * t + a2) * t + a1) * t + a0;
+                    Y = ((b3 * t + b2) * t + b1) * t + b0;
+                    if (first == 1)
+                    {
+                        first = 0;
+                    }
+                    else
+                    {
+                        Draw(Xold, Yold, X, Y);
+                    }
+                    Xold = X;
+                    Yold = Y;
+                }
+            }
+        }
+    }
+}
