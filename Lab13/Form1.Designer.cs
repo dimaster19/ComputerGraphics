3217dc587ffbb5dd83b08414fe6dedf0d2740b5f
dimaster19 <91622671+dimaster19@users.noreply.github.com>
2022-04-24 15:15:31 +0300

    Add files via upload

---
+++ Lab13/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace Lab13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(33, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(708, 532);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(230, 550);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(245, 41);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Старт";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(773, 259);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(204, 29);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(769, 209);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Введите число вершин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(770, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "(От 20 до 500)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1003, 603);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximumSize = new System.Drawing.Size(1019, 642);
+            this.MinimumSize = new System.Drawing.Size(1019, 642);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Lab13/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab13
+{
+    public partial class Form1 : Form
+    {
+        const int NMAX = 500;
+        const double BIG = 1.0e30;
+        Graphics graphics;
+        Pen pen;
+        int n;
+        int[] v;
+        double[] x;
+        double[] y;
+        //ComboBox tbComboBox1;
+        public const int bh = 40;
+        public const int bw = 40;
+        public static int iRows = 20, iColumns = 2;
+        public TextBox[,] tbArray;
+        public Form1()
+        {
+            InitializeComponent();
+            v = new int[NMAX];
+            x = new double[NMAX];
+            y = new double[NMAX];
+            graphics = pictureBox1.CreateGraphics();
+            pen = new Pen(Brushes.Black, 2);
+            //tbComboBox1 = new ComboBox()
+            //{
+            //    Location = new Point(61, 31),
+            //    Width = 121,
+            //    Height = 21
+            //};
+            //panel1.Controls.Add(tbComboBox1);
+            //for (int i = 0; i < NMAX; i++) 
+            //{ 
+            //    tbComboBox1.Items.Add(20+i); 
+            //}
+            //tbComboBox1.SelectedItem = 20;
+            ////tbComboBox1.SelectedIndexChanged += new EventHandler(UpdateBoxes);
+
+        }
+
+        private int IX(double x)
+        {
+            double xx = x * (pictureBox1.Size.Width / 10.0) + 0.5;
+            return (int)xx;
+        }
+        private int IY(double y)
+        {
+            double yy = pictureBox1.Size.Height - y * (pictureBox1.Size.Height / 7.0) + 0.5;
+            return (int)yy;
+        }
+        private void Draw(double x1, double y1, double x2, double y2)
+        {
+            Point point1 = new Point(IX(x1), IY(y1)); Point point2 = new Point(IX(x2), IY(y2));
+            graphics.DrawLine(pen, point1, point2);
+        }
+        private unsafe bool CounterClock(int h, int i, int j, double* pdist)
+        {
+            double xh = x[v[h]], xi = x[v[i]], xj = x[v[j]],
+            yh = y[v[h]], yi = y[v[i]], yj = y[v[j]],
+            x_hi, y_hi, x_hj, y_hj, Determ;
+            x_hi = xi - xh; y_hi = yi - yh; x_hj = xj - xh; y_hj = yj - yh;
+            *pdist = x_hj * x_hj + y_hj * y_hj;
+            Determ = x_hi * y_hj - x_hj * y_hi;
+            return (Determ > 1e-6);
+        }
+        private void DrawPolygon()
+        {
+            int i; double xold, yold;
+            xold = x[n - 1]; yold = y[n - 1];
+            for (i = 0; i < n; i++)
+            {
+                Draw(xold, yold, x[i], y[i]); xold = x[i]; yold = y[i];
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool finish = true;
+            while (finish)
+            {
+                try
+                {
+                    Triangulate(true);
+                    finish = false;
+                }
+                catch
+                {
+                    pen.DashStyle = DashStyle.Solid;
+                }
+            }
+            MessageBox.Show("Готово!", "", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+
+        private unsafe void Triangulate(bool randomly)
+        {
+
+            graphics.Clear(Color.White);
+            int i, h, j, m, k, imin = 0;
+            double diag, min_diag;
+            n = Convert.ToInt16(textBox1.Text);
+            if (n >= NMAX || n < 20)
+            {
+                MessageBox.Show("Неверное количество вершин!\nДиапазон от 20 до 500", "Ошибка",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            else
+            {
+                Random random = new Random();
+                for (i = 0; i < n; i++)
+                {
+                    if (randomly)
+                    {
+                        x[i] = Convert.ToDouble((random.Next(0, 9) + random.NextDouble()).ToString());
+                        y[i] = Convert.ToDouble((random.Next(0, 6) + random.NextDouble()).ToString());
+                    }
+
+                    v[i] = i;
+                }
+
+            }
+            m = n;
+            DrawPolygon();
+            float[] dashValues = { 5, 5, 5, 5 };
+            pen.DashPattern = dashValues;
+            while (m > 3)
+            {
+                min_diag = BIG;
+                for (i = 0; i < m; i++)
+                {
+                    h = (i == 0 ? m - 1 : i - 1); j = (i == m - 1 ? 0 : i + 1);
+                    if (CounterClock(h, i, j, &diag) && (diag < min_diag))
+                    {
+                        min_diag = diag; imin = i;
+                    }
+                }
+                i = imin; h = (i == 0 ? m - 1 : i - 1);
+                j = (i == m - 1 ? 0 : i + 1);
+                if (min_diag == BIG)
+                {
+                    if (!randomly)
+                    {
+                        MessageBox.Show("Неправильное направление обхода!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    throw new Exception();
+                }
+                Draw(x[v[h]], y[v[h]], x[v[j]], y[v[j]]);
+                m--;
+                for (k = i; k < m; k++)
+                {
+                    v[k] = v[k + 1];
+                }
+            }
+            pen.DashStyle = DashStyle.Solid;
+        }
+    }
+}
